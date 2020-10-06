53027e90aa5fa08ff58e54e18b5ffe81612cd2db
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-10-06 23:05:45 +0300

    Установлены и подключены пакеты Router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Main from '../main/main';
 
 const App = ({placesCount}) => {
   return (
-    <Main placesCount={placesCount}/>
+
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main placesCount={placesCount} />
+          <h1>MAIN</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
