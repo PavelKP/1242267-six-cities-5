3b907c6e2adcf739a8fe86e6d6d3051b41ffb24b
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-10-26 09:00:15 +0300

    Сортировка вынесена в компонент

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -5,6 +5,7 @@ import Map from '../map/map';
 import Header from '../header/header';
 import CityList from '../city-list/city-list';
 import PlacesCount from '../places-count/places-count';
+import SortingTypes from '../sorting-types/sorting-types';
 import {CardType} from '../../const';
 import {connect} from 'react-redux';
 import {offersPropTypes, cityPropTypes} from '../../prop-types/prop-types';
@@ -31,21 +32,7 @@ const Main = ({offers, city}) => {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <PlacesCount offersFiltered={offersFiltered}/>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                  <li className="places__option" tabIndex="0">Price: low to high</li>
-                  <li className="places__option" tabIndex="0">Price: high to low</li>
-                  <li className="places__option" tabIndex="0">Top rated first</li>
-                </ul>
-              </form>
+              <SortingTypes />
               <div className="cities__places-list places__list tabs__content">
                 <OfferList type={CardType.MAIN} offers={offersFiltered}/>
               </div>
