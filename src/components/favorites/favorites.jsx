31c5c87beb9ceb3153fb6fa0f3f3363a5a504bda
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-11-10 15:43:39 +0300

    Добавлена заглушка для favorites

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import OfferList from '../offer-list/offer-list';
 import Header from '../header/header';
+import FavoritesEmpty from '../favorites-empty/favorites-empty';
 import {offersPropTypes} from '../../prop-types/prop-types';
 import {CardType} from '../../const';
 
 
 const Favorites = (props) => {
   const locations = new Set();
-  const bookmarked = props.offers.filter((card) => {
-    if (card.isBookmarked) {
-      locations.add(card.location);
+  const bookmarked = props.offers.filter((offer) => {
+    if (offer.isBookmarked) {
+      locations.add(offer.city.name);
     }
-    return card.isBookmarked;
+    return offer.isBookmarked;
   });
+
+  const isEmpty = bookmarked.length === 0;
 
   return (
     <div className="page">
@@ -23,30 +26,36 @@ const Favorites = (props) => {
         </Link>
       </Header>
 
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              {[...locations].map((city, i) => (
-                <li className="favorites__locations-items" key={`favorites-location-${i}`}>
-                  <div className="favorites__locations locations locations--current">
-                    <div className="locations__item">
-                      <a className="locations__item-link" href="#">
-                        <span>{city}</span>
-                      </a>
-                    </div>
-                  </div>
-                  <div className="favorites__places">
-                    <OfferList type={CardType.FAVORITES} offers={bookmarked.filter((offer) => offer.location === city)}/>
-                  </div>
-                </li>
-              ))}
+      {isEmpty
+        ? <FavoritesEmpty />
+        : <React.Fragment>
+          <main className="page__main page__main--favorites">
+            <div className="page__favorites-container container">
+              <section className="favorites">
+                <h1 className="favorites__title">Saved listing</h1>
+                <ul className="favorites__list">
+                  {[...locations].map((city, i) => (
+                    <li className="favorites__locations-items" key={`favorites-location-${i}`}>
+                      <div className="favorites__locations locations locations--current">
+                        <div className="locations__item">
+                          <a className="locations__item-link" href="#">
+                            <span>{city}</span>
+                          </a>
+                        </div>
+                      </div>
+                      <div className="favorites__places">
+                        <OfferList type={CardType.FAVORITES} offers={bookmarked.filter((offer) => offer.city.name === city)} />
+                      </div>
+                    </li>
+                  ))}
 
-            </ul>
-          </section>
-        </div>
-      </main>
+                </ul>
+              </section>
+            </div>
+          </main>
+        </React.Fragment>
+      }
+
       <footer className="footer container">
         <Link to="/" className="footer__logo-link" href="main.html">
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
