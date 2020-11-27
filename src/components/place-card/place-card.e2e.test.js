7ae7281a2796d5d39f05e0aaeaf8e21a82e341eb
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-11-27 15:38:30 +0300

    Добавлен e2e тест для place card

---
+++ src/components/place-card/place-card.e2e.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {PlaceCard} from './place-card';
+import {offers} from '../../__mocks__/mock-offers';
+
+configure({adapter: new Adapter()});
+
+it(`Should place card react on mouse enter/leave`, () => {
+  const onCardMouseOver = jest.fn();
+
+  const wrapper = shallow(
+      <PlaceCard
+        offer = {offers[0]}
+        onCardMouseOver = {onCardMouseOver}
+        classArticle={`cities__place-card`}
+        classImageWrapper={`cities__image-wrapper`}
+        imageWidth={260}
+        imageHeight={200}
+      />);
+
+  const card = wrapper.find(`.place-card`);
+  card.simulate(`mouseEnter`, {preventDefault: ()=>{}});
+  card.simulate(`mouseLeave`, {preventDefault: ()=>{}});
+
+  expect(onCardMouseOver).toHaveBeenCalledTimes(2);
+});
